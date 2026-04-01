361ffbc51260e810ba300f8aa8abae6d994615f1
David-Mangueira <79489676+Davidmtp58@users.noreply.github.com>
2026-04-01 16:14:55 -0300

    Add files 1.py and 2.py to Aula_5

---
+++ Aula_5/1.py
@@ -0,0 +1,9 @@
+#Leia um número inteiro e informe se ele é positivo ou negativo.
+num = int(input("Digite um numero: "))
+
+if num > 0:
+    print("Positivo")
+elif num < 0:
+    print("Negativo")
+else:
+    print("Zero")
---
+++ Aula_5/2.py
@@ -0,0 +1,4 @@
+#Leia um número (entrada padrão do input) e: Converta para inteiro; Exiba o dobro do valor.
+num = int(input("Digite um numero: "))
+dobro = num * 2
+print("O dobro de", num, "é", dobro)
